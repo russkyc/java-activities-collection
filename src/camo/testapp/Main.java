062d75e4312a6c720b51dc5a884ec625e4e2e6f1
russky <32549126+russkyc@users.noreply.github.com>
2022-02-23 18:04:35 +0800

    2/23/2022

--- src/camo/testapp/Main.java
+++ src/camo/testapp/Main.java
@@ -7,25 +7,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         Student student1 = new Student();
 
-        System.out.print("Enter First Name: ");
-        student1.setFirstName(scanner.nextLine());
-
-        System.out.print("Enter Last Name: ");
-        student1.setLastName(scanner.nextLine());
+        System.out.print("Enter Last Name, First Name (eg: Juan, Dela Cruz): ");
+        String[] studentName = String.valueOf(scanner.nextLine()).trim().split(",");
+        student1.setFirstName(studentName[1]);
+        student1.setLastName(studentName[0]);
 
         System.out.print("Enter Address: ");
         student1.setAddress(scanner.nextLine());
 
-        System.out.print("Enter Student ID: ");
-        student1.setStudentId(scanner.nextLine());
+        System.out.print("Enter Student ID - Course(eg: 123456-BSIT): ");
+        String[] studentInfo = String.valueOf(scanner.nextLine()).trim().split("-");
+        student1.setStudentId(studentInfo[0]);
+        student1.setCourse(studentInfo[1]);
 
-        System.out.print("Enter Course: ");
-        student1.setCourse(scanner.nextLine());
-
-        System.out.println("First Name: " + student1.getFirstName());
-        System.out.println("Last Name: " + student1.getLastName());
-        System.out.println("Address: " + student1.getAddress());
-        System.out.println("Student ID: " + student1.getStudentId());
-        System.out.println("Course: " + student1.getCourse());
+        System.out.printf("First Name: %s\n",student1.getFirstName());
+        System.out.printf("Last Name: %s\n",student1.getLastName());
+        System.out.printf("Address: %s\n",student1.getAddress());
+        System.out.printf("Student ID: %s\n",student1.getStudentId());
+        System.out.printf("Course: %s\n",student1.getCourse());
     }
 }
